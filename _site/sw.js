9c95baa66f9b0168b0bccf26d253f3f5d9779ae1
Gzxl <gzx1990@gmail.com>
2023-02-24 10:32:03 +0800

    add _sit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230224.094117';
+const cacheName = 'chirpy-20230224.103046';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
